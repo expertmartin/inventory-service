47076410182837975e73373fe2f583e9394ed79e
expertmartin <martin.info.expert@gmail.com>
2021-09-08 00:38:02 -0400

    refactored JsonUtil.

--- src/main/java/com/kingtan/util/JsonUtil.java
+++ src/main/java/com/kingtan/util/JsonUtil.java
@@ -16,45 +16,44 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kingtan.store.inventory.domain.Product;
 
-
 public class JsonUtil {
+	private JsonUtil() {}
 
-	static public JsonNode getJsonNodeFromString(String jsonStr) throws JsonProcessingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readTree(jsonStr);
+	public static JsonNode getJsonNodeFromString(String jsonStr) throws JsonProcessingException, IOException {
+		return new ObjectMapper().readTree(jsonStr);
 	}
 
-	static public JsonNode getJsonNodeFromByte(byte[] jsonByte) throws JsonProcessingException, IOException {
+	public static JsonNode getJsonNodeFromByte(byte[] jsonByte) throws JsonProcessingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.readTree(jsonByte);
 	}
 
-	static public JsonNode getJsonNodeFromFile(File jsonFile) throws JsonProcessingException, IOException {
+	public static JsonNode getJsonNodeFromFile(File jsonFile) throws JsonProcessingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.readTree(jsonFile);
 	}
 
-	static public JsonNode getJsonNodeFromStream(InputStream jsonStream) throws JsonProcessingException, IOException {
+	public static JsonNode getJsonNodeFromStream(InputStream jsonStream) throws JsonProcessingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.readTree(jsonStream);
 	}
 
-	static public JsonNode getJsonNodeFromReader(Reader jsonReader) throws JsonProcessingException, IOException {
+	public static JsonNode getJsonNodeFromReader(Reader jsonReader) throws JsonProcessingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.readTree(jsonReader);
 	}
 
-	static public JsonNode getJsonNodeFromURL(URL jsonURL) throws JsonProcessingException, IOException {
+	public static JsonNode getJsonNodeFromURL(URL jsonURL) throws JsonProcessingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.readTree(jsonURL);
 	}
 
-	static public JsonNode getJsonNodeFromMap(Map<String, String> jmap) throws JsonProcessingException, IOException {
+	public static JsonNode getJsonNodeFromMap(Map<String, String> jmap) throws JsonProcessingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.readTree(mapper.writeValueAsString(jmap));
 	}
 
-	static public Map<String, List<Map<String, String>>> getMapSLMSS(String str)
+	public static Map<String, List<Map<String, String>>> getMapSLMSS(String str)
 			throws JsonProcessingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode jsonNode = mapper.readTree(str);
@@ -64,7 +63,7 @@ public class JsonUtil {
 		return result;
 	}
 
-	static public Map<String, List<Map<String, String>>> getMapSLMSS2(String str)
+	public static Map<String, List<Map<String, String>>> getMapSLMSS2(String str)
 			throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 
@@ -74,7 +73,7 @@ public class JsonUtil {
 		return map;
 	}
 
-	static public Map<String, List<Product>> getMapSLProduct(String str)
+	public static Map<String, List<Product>> getMapSLProduct(String str)
 			throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 
